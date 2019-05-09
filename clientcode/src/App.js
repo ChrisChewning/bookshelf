a8e5234152e0d988f5e84195a5dbbd0e264b055c
Chris <chewning.cl@gmail.com>
2019-05-09 06:59:24 -0500

    BookList component rendering on screen

--- clientcode/src/App.js
+++ clientcode/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import BookList from './components/BookList';
+import React, {Component } from 'react';
 import ApolloClient from 'apollo-boost';
-import ApolloProvider from 'react-apollo'; //glue layer.
+import { ApolloProvider } from 'react-apollo'; //glue layer.
+
+import BookList from './components/BookList';
 
 //apollo client setup to make requests to graphql endpoint.
 const client = new ApolloClient({
@@ -9,14 +10,15 @@ const client = new ApolloClient({
 })
 
 
-function App() {
+class App extends Component{
+  render(){
   return (
     <ApolloProvider client={client}>
-    <div id="main">
-      <h1>Kids Book List</h1>
-    <BookList />
-    </div>
-  </ApolloProvider>
+      <div id="main">
+        <h1>Kids Book List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
  }
 }
--- clientcode/src/components/BookList.js
+++ clientcode/src/components/BookList.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getBooksQuery = gql`
+  {
+    books{
+      name
+      id
+    }
+  }
+`
 
 class BookList extends Component {
+   displayBooks(){
+     var data = this.props.data;
+     if(data.loading){
+       return (<div>Loading books...</div>)
+     } else {
+       return data.books.map(book => {
+         return(
+         <li key={book.id}>{book.name}</li>
+       )
+      })
+     }
+   }
+
   render(){
+    console.log(this.props);
     return(
       <div id>
         <ul id="book-list">
-          <li>Book name</li>
+          {this.displayBooks()}
        </ul>
       </div>
     )
@@ -13,4 +38,7 @@ class BookList extends Component {
 
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
+//bind this query to this component. now inside the component you have access to this data. it is stored in the component's props. you can console.log(this.props)
+
+//this.display.books   this = component. displayBooks() is the method inside.
